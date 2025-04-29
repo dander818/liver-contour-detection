15b6c14a271d5d798614d14fc8c68cc1cee7a421
Данил Щербаков <danilshcherbakov@MacBook-Air-Danil.local>
2025-04-29 15:28:38 +0500

    feat: Implement liver contour prediction and related updates

--- images/urls.py
+++ images/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('images/', views.image_list, name='image_list'),
+    path('images/original/', views.original_image_list, name='original_image_list'),
+    path('images/processed/', views.processed_image_list, name='processed_image_list'),
     path('images/upload/', views.image_upload, name='image_upload'),
     path('images/<int:image_id>/', views.image_detail, name='image_detail'),
     path('images/<int:image_id>/delete/', views.image_delete, name='image_delete'),
+    path('images/<int:image_id>/process/', views.process_image, name='process_image'),
 ] 
